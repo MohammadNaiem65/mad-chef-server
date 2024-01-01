4b51cb125e5e80aadb51a15ccbd85cda5b9f1be6
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-01-01 12:15:28 +0600

    Added: created checkAuth middleware to authenticate a request

---
+++ middlewares/checkAuth.js
@@ -0,0 +1,23 @@
+const jwt = require('jsonwebtoken');
+
+const checkAuth = (req, res, next) => {
+	const authToken = req.headers?.authorization;
+	const token = authToken && authToken.split(' ')[1];
+
+	// send unauthorized response
+	if (!token) return res.sendStatus(401);
+
+	// verify the token
+	jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+		if (!user) {
+			return res.status(403).json({ msg: err.message });
+		}
+
+		// save user in request body
+		req.user = user;
+	});
+
+	next();
+};
+
+module.exports = checkAuth;
