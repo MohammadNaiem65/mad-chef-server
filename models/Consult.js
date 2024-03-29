9e5a45d74ae316fc3861e69079f393950cb6d0dc
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-30 01:12:46 +0600

    Added: created Consult mongoose model

---
+++ models/Consult.js
@@ -0,0 +1,42 @@
+const { default: mongoose } = require('mongoose');
+
+const consultSchema = new mongoose.Schema({
+	userId: {
+		type: mongoose.SchemaTypes.ObjectId,
+		ref: 'User',
+		required: true,
+	},
+	username: {
+		type: String,
+		required: true,
+	},
+	userEmail: {
+		type: String,
+		required: true,
+	},
+	chefId: {
+		type: mongoose.SchemaTypes.ObjectId,
+		ref: 'Chef',
+		required: true,
+	},
+	chefName: {
+		type: String,
+		required: true,
+	},
+	date: {
+		type: Date,
+		required: true,
+	},
+	startTime: {
+		type: String,
+		required: true,
+	},
+	endTime: {
+		type: String,
+		required: true,
+	},
+});
+
+const Consult = mongoose.model('Consult', consultSchema);
+
+module.exports = Consult;
