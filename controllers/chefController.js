a6c3a91f963370936a527c248017da0090f49f52
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-03-29 18:37:09 +0600

    Updated: made the /chefs endpoint functional with basic filtering

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -11,4 +11,24 @@ async function getChef(req, res) {
 	res.json({ data: result });
 }
 
-module.exports = { getChef };
+async function getChefs(req, res) {
+	const { p, page = 0, l, limit = 10 } = req.query;
+
+	const _page = parseInt(p || page) - 1;
+	const _limit = parseInt(l || limit);
+
+	try {
+		const result = await Chef.find()
+			.sort({ rating: 'desc' })
+			.skip((_page > 0 ? _page : 0) * _limit)
+			.limit(_limit);
+
+		const totalChefs = await Chef.countDocuments({});
+
+		res.json({ data: result, meta: { totalCount: totalChefs } });
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+module.exports = { getChef, getChefs };
--- handlers/chefHandler.js
+++ handlers/chefHandler.js
@@ -1,11 +1,12 @@
 const express = require('express');
 
 // internal imports
-const { getChef } = require('../controllers/chefController');
+const { getChef, getChefs } = require('../controllers/chefController');
 
 // create router instance
 const router = express.Router();
 
+router.get('/', getChefs);
 router.get(['/chef/:chefId', '/:chefId'], getChef);
 
 module.exports = router;
