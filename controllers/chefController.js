4ac706693c3fb2f0e3de91b736e0226e3bb3e1fc
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-02 08:13:50 +0600

    Updated: included rating info with chef result details

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -1,5 +1,10 @@
-const Chef = require('../models/Chef');
+const { default: mongoose } = require('mongoose');
+const { ObjectId } = mongoose.Types;
+
 const getCurrPage = require('../utility/getCurrPage');
+
+const Chef = require('../models/Chef');
+const ChefReview = require('../models/ChefReview');
 
 async function getChef(req, res) {
 	const { chefId } = req.params;
@@ -8,8 +13,45 @@ async function getChef(req, res) {
 		return res.status(400).json({ message: 'Provide valid chef id.' });
 	}
 
-	const result = await Chef.findById(chefId);
-	res.json({ data: result });
+	try {
+		const chef = await Chef.findById(chefId);
+
+		const rating = await ChefReview.aggregate([
+			{
+				$match: {
+					chefId: new ObjectId(chefId),
+				},
+			},
+			{
+				$group: {
+					_id: '$chefId',
+					rating: {
+						$avg: '$rating',
+					},
+					totalCount: {
+						$sum: 1,
+					},
+				},
+			},
+		]);
+
+		if (chef?._id) {
+			const result = {
+				...chef?._doc,
+				rating: rating?.length
+					? Number.parseFloat(rating[0]?.rating.toFixed(1))
+					: null,
+				totalRating: rating?.length ? rating[0]?.totalCount : null,
+			};
+
+			res.json({ message: 'Successful', data: result });
+		} else {
+			res.status(404).send('Something went wrong. Kindly try again!');
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
 }
 
 async function getChefs(req, res) {
