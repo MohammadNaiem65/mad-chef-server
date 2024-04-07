e634fce8f32b284f26db2a6df38c734710a37e20
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-07 16:12:32 +0600

    add: created getRecipes function to fetch recipe documents from database optimistically

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -1,5 +1,7 @@
 const { default: mongoose } = require('mongoose');
 const { ObjectId } = mongoose.Types;
+
+const getCurrPage = require('../utility/getCurrPage');
 const Recipe = require('../models/Recipe');
 const Rating = require('../models/Rating');
 
@@ -71,4 +73,113 @@ async function postRecipe(req, res) {
 	}
 }
 
-module.exports = { getRecipe, postRecipe };
+async function getRecipes(req, res) {
+	const {
+		p,
+		page = 0,
+		l,
+		limit = process.env.RECIPES_PER_PAGE,
+		sort = 'updatedAt',
+		order = 'desc',
+		include = '',
+		exclude = '',
+	} = req.query;
+
+	const _page = parseInt(p || page) - 1;
+	const _limit = parseInt(l || limit) <= 0 ? 1 : parseInt(l || limit);
+
+	// Initialize pipeline options as an empty object
+	let projection = {};
+	let includesObj = {};
+	let excludesObj = {};
+	const sortObj = {};
+
+	// set sort order field based on query
+	sort.split(',').map((el) => (sortObj[el] = order === 'desc' ? -1 : 1));
+
+	// Only create projection objects if include or exclude is not an empty string
+	if (include) {
+		includesObj = createProjectionObject(include, {}, 1);
+		projection = { ...projection, ...includesObj };
+	}
+	if (exclude) {
+		excludesObj = createProjectionObject(exclude, {}, 0);
+		projection = { ...projection, ...excludesObj };
+	}
+
+	// create the aggregation pipeline
+	const pipeline = [
+		{ $sort: sortObj },
+		{ $skip: (_page <= 0 ? 0 : _page) * _limit },
+		{ $limit: _limit },
+	];
+
+	// add stage to calculate average rating
+	if (includesObj?.rating || !excludesObj?.rating) {
+		if (sortObj?.rating) {
+			pipeline.unshift(
+				{
+					$lookup: {
+						from: 'ratings',
+						localField: '_id',
+						foreignField: 'recipeId',
+						as: 'rating',
+					},
+				},
+				{
+					$addFields: {
+						rating: {
+							$round: [{ $avg: '$rating.rating' }, 2],
+						},
+					},
+				}
+			);
+		} else {
+			pipeline.push(
+				{
+					$lookup: {
+						from: 'ratings',
+						localField: '_id',
+						foreignField: 'recipeId',
+						as: 'rating',
+					},
+				},
+				{
+					$addFields: {
+						rating: {
+							$round: [{ $avg: '$rating.rating' }, 2],
+						},
+					},
+				}
+			);
+		}
+	}
+
+	// add projection stage if needed
+	if (Object.keys(projection).length > 0) {
+		pipeline.push({ $project: projection });
+	}
+
+	try {
+		const result = await Recipe.aggregate(pipeline);
+
+		const totalRecipes = await Recipe.countDocuments({});
+		const currPage = getCurrPage(
+			_page <= 0 ? 1 : _page + 1,
+			_limit,
+			totalRecipes
+		);
+
+		res.json({
+			data: result,
+			meta: {
+				page: currPage,
+				totalCount: totalRecipes,
+			},
+		});
+	} catch (error) {
+		console.log(error);
+	}
+}
+
+module.exports = { getRecipe, postRecipe, getRecipes };
--- handlers/recipeHandler.js
+++ handlers/recipeHandler.js
@@ -1,12 +1,17 @@
 const express = require('express');
 
 const checkAuth = require('../middlewares/checkAuth');
-const { getRecipe, postRecipe } = require('../controllers/recipeController');
+const {
+	getRecipe,
+	postRecipe,
+	getRecipes,
+} = require('../controllers/recipeController');
 
 // Create router instance
 const router = express.Router();
 
-router.post('/recipe', checkAuth, postRecipe);
 router.get(['/recipe/:recipeId', '/:recipeId'], getRecipe);
+router.post('/recipe', checkAuth, postRecipe);
+router.get('/', getRecipes);
 
 module.exports = router;
