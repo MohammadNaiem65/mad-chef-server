6ab6e476b866f087124724220c76ed0ef5f7acd8
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-22 14:44:02 +0600

    add: created validateMongoDBId utility function to validate id

---
+++ utility/validateMongoDBId.js
@@ -0,0 +1,17 @@
+const { isValidObjectId } = require('mongoose');
+
+/**
+ * Validates if a given string is a valid MongoDB ObjectId.
+ *
+ * @param {string} id - The string to validate as a MongoDB ObjectId.
+ * @param {object} res - The Express response object to send a 400 error if the ID is invalid.
+ * @returns {void} - This function does not return a value. Instead, it sends a 400 error response if the ID is invalid.
+ */
+function validateMongoDBId(id, res) {
+	if (!isValidObjectId(id)) {
+		// Send a 400 response indicating the ID is invalid
+		return res.status(400).send({ error: 'Invalid MongoDB ID provided.' });
+	}
+}
+
+module.exports = validateMongoDBId;
