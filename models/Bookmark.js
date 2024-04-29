02f7a0e357245449a7512f40d88b28904d3e07cb
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-04-29 08:30:01 +0600

    update: added timestamps to Bookmark model

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,15 +1,20 @@
 const { default: mongoose } = require('mongoose');
 
-const bookmarkSchema = new mongoose.Schema({
-	userId: {
-		type: mongoose.SchemaTypes.ObjectId,
-		ref: 'User',
+const bookmarkSchema = new mongoose.Schema(
+	{
+		userId: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: 'User',
+		},
+		recipeId: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: 'Recipe',
+		},
 	},
-	recipeId: {
-		type: mongoose.SchemaTypes.ObjectId,
-		ref: 'Recipe',
-	},
-});
+	{
+		timestamps: true,
+	}
+);
 
 const Bookmark = mongoose.model('Bookmark', bookmarkSchema);
 module.exports = Bookmark;
