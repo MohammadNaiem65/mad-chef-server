3dbe5d4406521b2827cc01c290d95c2b72d2fa78
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-21 02:29:38 +0600

    fix: adjusted recipeController middlewares

--- controllers/recipeController.js
+++ controllers/recipeController.js
@@ -7,82 +7,6 @@ const Recipe = require('../models/Recipe');
 const Rating = require('../models/Rating');
 const validateMongoDBId = require('../utility/validateMongoDBId');
 
-async function getRecipe(req, res) {
-	const { recipeId } = req.params;
-
-	if (!recipeId) {
-		return res
-			.status(400)
-			.json({ message: 'An ID is required to get a recipe.' });
-	}
-
-	validateMongoDBId(recipeId, res);
-
-	try {
-		const recipe = await Recipe.findById(recipeId);
-		const rating = await Rating.aggregate([
-			{
-				$match: {
-					recipeId: new ObjectId(recipeId),
-				},
-			},
-			{
-				$group: {
-					_id: '$recipeId',
-					rating: {
-						$avg: '$rating',
-					},
-					totalCount: {
-						$sum: 1,
-					},
-				},
-			},
-		]);
-
-		if (recipe?._id) {
-			const result = {
-				...recipe?._doc,
-				rating: rating?.length
-					? Number.parseFloat(rating[0]?.rating.toFixed(1))
-					: null,
-				totalRating: rating?.length ? rating[0]?.totalCount : null,
-			};
-
-			res.json({
-				message: 'Successful',
-				data: result,
-			});
-		} else {
-			res.status(404).send('Something went wrong. Kindly try again!');
-		}
-	} catch (err) {
-		console.log(err);
-		res.status(500).json(err);
-	}
-}
-
-async function postRecipe(req, res) {
-	const { userId, role } = req.user;
-	const { title, ingredients, method, img, author } = req.body;
-
-	// Validate the userId
-	validateMongoDBId(userId, res);
-
-	if (userId && role === 'chef') {
-		const doc = await Recipe.create({
-			title,
-			ingredients,
-			method,
-			img,
-			author,
-		});
-
-		res.json({ message: 'Successful', data: doc });
-	} else {
-		res.status(401).json({ message: 'Only chef can post recipe.' });
-	}
-}
-
 async function searchRecipes(req, res) {
 	const {
 		p,
@@ -340,6 +264,84 @@ async function searchRecipes(req, res) {
 	} catch (error) {
 		console.log(error);
 		res.json(error);
+	}
+}
+
+async function getRecipe(req, res) {
+	const { recipeId } = req.params;
+
+	if (!recipeId) {
+		return res
+			.status(400)
+			.json({ message: 'An ID is required to get a recipe.' });
+	}
+
+	if (!validateMongoDBId(recipeId, res)) {
+		return;
+	}
+
+	try {
+		const recipe = await Recipe.findById(recipeId);
+		const rating = await Rating.aggregate([
+			{
+				$match: {
+					recipeId: new ObjectId(recipeId),
+				},
+			},
+			{
+				$group: {
+					_id: '$recipeId',
+					rating: {
+						$avg: '$rating',
+					},
+					totalCount: {
+						$sum: 1,
+					},
+				},
+			},
+		]);
+
+		if (recipe?._id) {
+			const result = {
+				...recipe?._doc,
+				rating: rating?.length
+					? Number.parseFloat(rating[0]?.rating.toFixed(1))
+					: null,
+				totalRating: rating?.length ? rating[0]?.totalCount : null,
+			};
+
+			res.json({
+				message: 'Successful',
+				data: result,
+			});
+		} else {
+			res.status(404).send('Something went wrong. Kindly try again!');
+		}
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+}
+
+async function postRecipe(req, res) {
+	const { userId, role } = req.user;
+	const { title, ingredients, method, img, author } = req.body;
+
+	// Validate the userId
+	validateMongoDBId(userId, res);
+
+	if (userId && role === 'chef') {
+		const doc = await Recipe.create({
+			title,
+			ingredients,
+			method,
+			img,
+			author,
+		});
+
+		res.json({ message: 'Successful', data: doc });
+	} else {
+		res.status(401).json({ message: 'Only chef can post recipe.' });
 	}
 }
 
