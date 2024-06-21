65a688f2c9991283fd55e6dba098076a840ec9bc
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-21 16:43:28 +0600

    update: added timestamps option to ChefReview model

--- models/ChefReview.js
+++ models/ChefReview.js
@@ -1,29 +1,32 @@
 const mongoose = require('mongoose');
 
-const chefReviewSchema = new mongoose.Schema({
-	chefId: {
-		type: mongoose.SchemaTypes.ObjectId,
-		ref: 'Chef',
-		required: true,
+const chefReviewSchema = new mongoose.Schema(
+	{
+		chefId: {
+			type: mongoose.SchemaTypes.ObjectId,
+			ref: 'Chef',
+			required: true,
+		},
+		userId: {
+			type: mongoose.Schema.Types.ObjectId,
+			ref: 'User',
+			required: true,
+		},
+		rating: {
+			type: Number,
+			required: true,
+			min: [0, 'The rating must be between 0 and 5'],
+			max: [5, 'The rating must be between 0 and 5'],
+		},
+		message: {
+			type: String,
+			required: true,
+		},
 	},
-	userId: {
-		type: mongoose.Schema.Types.ObjectId,
-		ref: 'User',
-		required: true,
-	},
-	rating: {
-		type: Number,
-		required: true,
-		min: [0, 'The rating must be between 0 and 5'],
-		max: [5, 'The rating must be between 0 and 5'],
-	},
-	message: {
-		type: String,
-		required: true,
-	},
-});
+	{ timestamps: true }
+);
 
-// indexing userId and chefId fields so that they always
+// Indexing userId and chefId fields so that they always unique
 chefReviewSchema.index({ userId: 1, chefId: 1 }, { unique: true });
 
 const ChefReview = mongoose.model('ChefReview', chefReviewSchema);
