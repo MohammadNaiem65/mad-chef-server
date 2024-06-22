8e9a9ff922b715e1b4eb5dc9f58519d4a56155cd
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-06-22 20:32:45 +0600

    add: created getChefReviews middleware to fetch chef reviews

--- controllers/chefController.js
+++ controllers/chefController.js
@@ -5,6 +5,8 @@ const getCurrPage = require('../utility/getCurrPage');
 const createProjectionObject = require('../utility/createProjectionObject');
 
 const Chef = require('../models/Chef');
+const ChefReview = require('../models/ChefReview');
+const createSortObject = require('../utility/createSortObject');
 
 async function getChef(req, res) {
 	const { chefId } = req.params;
@@ -88,16 +90,15 @@ async function getChefs(req, res) {
 	} = req.query;
 
 	const _page = parseInt(p || page) - 1;
-	const _limit = parseInt(l || limit) <= 0 ? 1 : parseInt(l || limit);
+	const _limit = parseInt(l || limit) <= 0 ? 10 : parseInt(l || limit);
 
 	// Initialize pipeline options as an empty object
 	let projection = {};
 	let includesObj = {};
 	let excludesObj = {};
-	const sortObj = {};
 
-	// set sort order field based on query
-	sort.split(',').map((el) => (sortObj[el] = order === 'desc' ? -1 : 1));
+	// Create sort object based on query
+	const sortObj = createSortObject(sort, order);
 
 	// Only create projection objects if include or exclude is not an empty string
 	if (include) {
@@ -109,14 +110,14 @@ async function getChefs(req, res) {
 		projection = { ...projection, ...excludesObj };
 	}
 
-	// create the aggregation pipeline
+	// Create the aggregation pipeline
 	const pipeline = [
 		{ $sort: sortObj },
 		{ $skip: (_page <= 0 ? 0 : _page) * _limit },
 		{ $limit: _limit },
 	];
 
-	// add stage to calculate average rating
+	// Add stage to calculate average rating
 	if (includesObj?.rating || !excludesObj?.rating) {
 		if (sortObj?.rating) {
 			pipeline.unshift(
@@ -157,7 +158,7 @@ async function getChefs(req, res) {
 		}
 	}
 
-	// add projection stage if needed
+	// Add projection stage if needed
 	if (Object.keys(projection).length > 0) {
 		pipeline.push({ $project: projection });
 	}
@@ -179,9 +180,52 @@ async function getChefs(req, res) {
 				totalCount: totalRecipes,
 			},
 		});
-	} catch (error) {
-		console.log(error);
+	} catch (err) {
+		res.status(500).json(err);
 	}
 }
 
-module.exports = { getChef, getChefs };
+async function getChefReviews(req, res) {
+	const { chefId } = req.params;
+	const {
+		p,
+		page = 0,
+		l,
+		limit = process.env.CHEF_REVIEWS_PER_PAGE,
+		sort = 'updatedAt',
+		order = 'desc',
+	} = req.query;
+
+	const _page = parseInt(p || page) - 1;
+	const _limit = parseInt(l || limit) <= 0 ? 10 : parseInt(l || limit);
+
+	// Create sort object based on query
+	const sortObj = createSortObject(sort, order);
+
+	try {
+		const result = await ChefReview.find({ chefId })
+			.sort(sortObj)
+			.skip((_page <= 0 ? 0 : _page) * _limit)
+			.limit(_limit);
+
+		const totalReviews = await ChefReview.countDocuments({ chefId });
+		const currPage = getCurrPage(
+			_page <= 0 ? 1 : _page + 1,
+			_limit,
+			totalReviews
+		);
+
+		res.json({
+			data: result,
+			meta: {
+				page: currPage,
+				totalCount: totalReviews,
+			},
+		});
+	} catch (err) {
+		console.log(err);
+		res.status(500).json(err);
+	}
+}
+
+module.exports = { getChef, getChefs, getChefReviews };
--- handlers/chefHandler.js
+++ handlers/chefHandler.js
@@ -1,13 +1,24 @@
 const express = require('express');
 
 // internal imports
-const { getChef, getChefs } = require('../controllers/chefController');
+const {
+	getChef,
+	getChefs,
+	getChefReviews,
+} = require('../controllers/chefController');
 const checkAuth = require('../middlewares/checkAuth');
+const checkChef = require('../middlewares/checkChef');
 
 // create router instance
 const router = express.Router();
 
 router.get('/', getChefs);
 router.get(['/chef/:chefId', '/:chefId'], getChef);
+router.get(
+	['/chef/:chefId/reviews', '/:chefId/reviews'],
+	checkAuth,
+	checkChef,
+	getChefReviews
+);
 
 module.exports = router;
