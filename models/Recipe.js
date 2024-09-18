8746f03930c23e3656ba2893ec084665b5578337
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-18 23:04:07 +0600

    update: added status field into Recipe model

--- models/Recipe.js
+++ models/Recipe.js
@@ -1,53 +1,58 @@
 const mongoose = require('mongoose');
 
 const recipeSchema = new mongoose.Schema(
-	{
-		title: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		ingredients: {
-			type: [String],
-			required: true,
-		},
-		region: {
-			type: String,
-			enum: [
-				'asia',
-				'europe',
-				'america',
-				'latin america',
-				'africa',
-				'middle east',
-			],
-			required: true,
-		},
-		method: {
-			type: String,
-			required: true,
-			trim: true,
-		},
-		img: {
-			type: String,
-			required: true,
-		},
-		imgTitle: {
-			type: String,
-		},
-		like: {
-			type: Number,
-			default: 0,
-		},
-		author: {
-			type: mongoose.SchemaTypes.ObjectId,
-			ref: 'Chef',
-			required: true,
-		},
-	},
-	{
-		timestamps: true,
-	}
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        ingredients: {
+            type: [String],
+            required: true,
+        },
+        region: {
+            type: String,
+            enum: [
+                'asia',
+                'europe',
+                'america',
+                'latin america',
+                'africa',
+                'middle east',
+            ],
+            required: true,
+        },
+        method: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        img: {
+            type: String,
+            required: true,
+        },
+        imgTitle: {
+            type: String,
+        },
+        like: {
+            type: Number,
+            default: 0,
+        },
+        status: {
+            type: String,
+            enum: ['pending', 'published', 'hidden', 'rejected'],
+            required: true,
+        },
+        author: {
+            type: mongoose.SchemaTypes.ObjectId,
+            ref: 'Chef',
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
 );
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
