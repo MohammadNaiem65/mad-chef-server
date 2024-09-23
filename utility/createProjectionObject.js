1fee2e4060d68162baac98e029f2554e90dca009
Md Rifat Ahmed Tusar <mohammadnaiem661065@gmail.com>
2024-09-23 21:41:28 +0600

    update: updated the createProjectionObject utility function to handle both include and exclude parameter at the same time

--- utility/createProjectionObject.js
+++ utility/createProjectionObject.js
@@ -1,29 +1,32 @@
 /**
- * @description - this function takes a string, object and a value, and returns an object where the keys of the object are from string (the keys are separated by comma) and the value of the keys will be the third parameter
+ * Creates a projection object for MongoDB queries.
+ * The projection object specifies which fields to include or exclude in the query results.
  *
- * @param {string} projectionString - a string representation of the projection fields separated by commas
- * @param {object} projectionObject - a projection object where the projection values will be saved
- * @param {number} projectionValue - a value (0 or 1) to save as the value of the projection object's fields
- * @returns {object}
+ * @param {string} [include] - Comma-separated list of fields to include in the projection.
+ * If provided, all other fields will be excluded.
+ *
+ * @param {string} [exclude] - Comma-separated list of fields to exclude from the projection.
+ * If provided, all other fields will be included.
+ *
+ * @returns {object} - A projection object where keys are field names and values are 0 (exclude) or 1 (include).
  *
  * @example
- * createProjectionObject('field1,field2', {}, 1); // { field1: 1, field2: 1 }
+ * // Include only 'name' and 'age' fields in the projection
+ * const projection = createProjectionObject('name,age');
+ * console.log(projection); // { name: 1, age: 1 }
+ *
+ * // Exclude 'password' and 'email' fields from the projection
+ * const projection = createProjectionObject(null, 'password, email');
+ * console.log(projection); // { password: 0, email: 0 }
  */
-function createProjectionObject(
-	projectionString = '',
-	projectionObject = {},
-	projectionValue
-) {
-	const fields = projectionString.split(',');
-
-	// Using Object.assign with Array.reduce for a more concise approach
-	return Object.assign(
-		projectionObject,
-		fields.reduce((acc, field) => {
-			acc[field] = projectionValue;
-			return acc;
-		}, {})
-	);
+function createProjectionObject(include, exclude) {
+    const projection = {};
+    if (exclude && !include) {
+        exclude.split(',').forEach((field) => (projection[field.trim()] = 0));
+    } else if (include && !exclude) {
+        include.split(',').forEach((field) => (projection[field.trim()] = 1));
+    }
+    return projection;
 }
 
 module.exports = createProjectionObject;
