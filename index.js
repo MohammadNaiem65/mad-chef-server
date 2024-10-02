6f90c7169225f417194e4c3565f710085da53ede
MohammadNaiem65 <mohammadnaiem661065@gmail.com>
2024-10-02 09:01:20 +0600

    Remove firebase.config.json from Git tracking

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-// external imports
+// External imports
 const express = require('express');
 const mongoose = require('mongoose');
-const { initializeApp, cert } = require('firebase-admin/app');
 const cors = require('cors');
-const cookieParser = require('cookie-parser');
 const cloudinary = require('cloudinary').v2;
+const cookieParser = require('cookie-parser');
+const { initializeApp, cert } = require('firebase-admin/app');
 require('dotenv').config();
 
-// internal imports
+// Internal imports
 // ? TODO: download firebase service account configuration and name firebase.config.json
 const serviceAccountConfig = require('./firebase.config.json');
 
-// request handlers
+// Request handlers
 const authHandler = require('./handlers/authHandler');
 const userHandler = require('./handlers/userHandler');
 const chefHandler = require('./handlers/chefHandler');
@@ -21,12 +21,12 @@ const consultHandler = require('./handlers/consultHandler');
 const paymentHandler = require('./handlers/paymentHandler');
 const roleHandler = require('./handlers/roleHandler');
 
-// create app instance
+// Create app instance
 const app = express();
 
 const port = process.env.PORT || 3999;
 
-// request parsers
+// Request parsers
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(
@@ -37,25 +37,30 @@ app.use(
 );
 app.use(cookieParser(process.env.COOKIE_SECRET));
 
-// connect to MongoDB
+// Connect to MongoDB
 mongoose
     .connect(process.env.MONGODB_CONNECTION_STRING)
     .then(() => console.log('Connected to database'))
     .catch((err) => console.log(err));
 
-// connect to Firebase
+// Connect to Firebase
 initializeApp({
     credential: cert(serviceAccountConfig),
 });
 
-// configure cloudinary
+// Configure cloudinary
 cloudinary.config({
     cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET,
 });
 
-// request endpoints
+// Root endpoint
+app.get('/', (req, res) => {
+    res.json({ message: 'Welcome to Mad Chef Server' });
+});
+
+// Request endpoints
 app.use('/auth', authHandler);
 app.use('/users', userHandler);
 app.use('/chefs', chefHandler);
@@ -65,7 +70,7 @@ app.use('/consults', consultHandler);
 app.use('/payments', paymentHandler);
 app.use('/roles', roleHandler);
 
-// listen app
+// Listen app
 app.listen(port, () => {
     console.log(`App listening on port ${port}`);
 });
